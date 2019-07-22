5ab4b1e2f6b17aa8d7ab8cc282dd4c483fb7c734
Jon Struve <ocdammo3@gmail.com>
2019-07-22 15:48:47 -0500

    Fixed js errors

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 "use strict";
 
-var imageNames = ["images/bag.jpg","images/banana.jpg","images/bathroom.jpg","images/boots.jpg","images/breakfast.jpg","images/bubblegum.jpg","images/chair.jpg","images/cthulhu.jpg","images/dog-duck.jpg","images/dragon.jpg","images/pen.jpg","images/pet-sweep.jpg","images/scissors.jpg","images/shark.jpg","images/sweep.png","images/tauntaun.jpg","images/unicorn.jpg","images/usb.gif","images/water-can.jpg","images/wine-glass.jpg"];
+var imageNames = [
+    ["bag", "banana", "bathroom", "boots", "breakfast", "bubblegum", "chair", "cthulhu", "dogDuck", "dragon", "pen", "petSweep", "scissors", "shark", "sweep", "tauntaun", "unicorn", "usb", "waterCan", "wineGlass"],
+    ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "usb.gif", "water-can.jpg", "wine-glass.jpg"],
+    ["Star Wars Bag", "Banana Slicer", "Bathroom TP and Tablet Holder", "All-in-One Breakfast Machine", "Meatball Bubblegum", "Bubble Chair", "Cthulhu", "Dog w/ Duck Mask", "Dragon Meat", "Silverware Pens", "Pet Sweeper Shoes", "Pizza Scissors", "Shark Sleeping Bag", "Baby Sweeper Outfit", "Star Wars Sleeping Bag", "Unicorn Meat", "Tentacle USB", "Illogical Watering Can", "Illogical Wine Glass"]
+];
 var allImages = [];
 var totalClicks = 0;
 
-function Image(name){
-    this.name = name;
+console.table(imageNames);
+
+function Image(){
+    this.imageURL = "images/";
+    this.name = "";
+    this.description = "";
     this.clicks = 0;
     this.views = 0;
     allImages.push(this);
 };
 
 function createImages(){
-    for(var i = 0; i < imageNames.length; i++){
+    for(var i = 0; i < imageNames[0].length; i++){
         new Image(imageNames[i]);
+        allImages[i].name = imageNames[0][i];
+        allImages[i].imageURL += imageNames[1][i];
+        allImages[i].description = imageNames[2][i];
     }
     console.table(allImages);
 }
@@ -40,8 +51,9 @@ function render(){
     for(var i = 0; i < 3; i++){
         allImages[randomImages[i]].views++;
         var img = document.createElement("img");
-        img.setAttribute("src", allImages[randomImages[i]].name);
-        img.setAttribute("data-type", allImages[randomImages[i]].name);
+        img.setAttribute("name", allImages[randomImages[i]].description);
+        img.setAttribute("src", allImages[randomImages[i]].imageURL);
+        img.setAttribute("data-name", allImages[randomImages[i]].name);
         img.addEventListener("click", handleVote);
         imagesSection.appendChild(img);
     }
@@ -49,6 +61,7 @@ function render(){
 
 function handleVote(event){
     var imageName = event.target.dataset.name;
+    console.log(imageName);
     for(var i = 0; i < allImages.length; i++){
         if(allImages[i].name === imageName){
             allImages[i].clicks++;
@@ -73,7 +86,7 @@ function showResults(){
     for(var i = 0; i < allImages.length; i++){
         var image = allImages[i];
         var li = document.createElement("li");
-        li.textContent = image.name + " received " + image.clicks + " votes.";
+        li.textContent = image.description + " received " + image.clicks + " votes.";
         ul.appendChild(li);
     }
     results.appendChild(ul);
