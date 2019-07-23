8863e31e51b051e27de34a17d80d69a402d5a7a9
Jon Struve <ocdammo3@gmail.com>
2019-07-23 16:34:08 -0500

    Finished chart, need to restlye page

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
 var imageNames = [
     ["bag", "banana", "bathroom", "boots", "breakfast", "bubblegum", "chair", "cthulhu", "dogDuck", "dragon", "pen", "petSweep", "scissors", "shark", "sweep", "tauntaun", "unicorn", "usb", "waterCan", "wineGlass"],
     ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "usb.gif", "water-can.jpg", "wine-glass.jpg"],
-    ["Star Wars Sleeping Bag", "Banana Slicer", "Bathroom TP and Tablet Holder", "Boots w/ Hole for Toes", "All-in-One Breakfast Machine", "Meatball Bubblegum", "Bubble Chair", "Cthulhu", "Dog w/ Duck Mask", "Dragon Meat", "Silverware Pens", "Pet Sweeper Shoes", "Pizza Scissors", "Shark Sleeping Bag", "Baby Sweeper Outfit", "Star Wars Sleeping Bag", "Unicorn Meat", "Tentacle USB", "Illogical Watering Can", "Illogical Wine Glass"]
+    ["Star Wars Suitcase", "Banana Slicer", "Bathroom TP and Tablet Holder", "Boots w/ Hole for Toes", "All-in-One Breakfast Machine", "Meatball Bubblegum", "Bubble Chair", "Cthulhu", "Dog w/ Duck Mask", "Dragon Meat", "Silverware Pens", "Pet Sweeper Shoes", "Pizza Scissors", "Shark Sleeping Bag", "Baby Sweeper Outfit", "Star Wars Sleeping Bag", "Unicorn Meat", "Tentacle USB", "Illogical Watering Can", "Illogical Wine Glass"]
 ];
 var allImages = [];
 var totalClicks = 0;
 var displayNumber = 3;
 var lastShown = [];
 var globalRandomImages = [];
+var colors = [];
+var colors2 = [];
 
 console.table(imageNames);
 
@@ -20,6 +22,8 @@ function Image(){
     this.clicks = 0;
     this.views = 0;
     allImages.push(this);
+    colors.push(getRandomRgba());
+    colors2.push(getRandomRgb());
 };
 
 function createImages(){
@@ -60,12 +64,15 @@ function render(){
 
     for(var i = 0; i < 3; i++){
         allImages[randomImages[i]].views++;
-        var img = document.createElement("img");
+        var imgDiv = document.createElement("div");
+        imgDiv.classList.add("imgDiv");
+                var img = document.createElement("img");
         img.setAttribute("name", allImages[randomImages[i]].description);
         img.setAttribute("src", allImages[randomImages[i]].imageURL);
         img.setAttribute("data-name", allImages[randomImages[i]].name);
         img.addEventListener("click", handleVote);
-        imagesSection.appendChild(img);
+        imgDiv.appendChild(img);
+        imagesSection.appendChild(imgDiv);
     }
     globalRandomImages = randomImages;
 }
@@ -91,39 +98,61 @@ function handleVote(event){
     console.table(allImages);
     console.log("Total Clicks", totalClicks);
 }
-
+function getRandomRgba() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return `rgba(${r},${g},${b},.4)`;
+}
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return `rgb(${r},${g},${b})`;
+}
+function createChart(ctx, labels, dataLabel, data) {
+    var canvas = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: dataLabel,
+                data: data,
+                backgroundColor: colors,
+                borderColor: colors2,
+                borderWidth: 3
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
 function showResults(){
-    var results = document.getElementById("results");
-    var ul = document.createElement("ul");
+    var canvas = document.createElement("canvas");
+    canvas.id = "canvas";
+    var context2d = canvas.getContext("2d");
+
+    var labels = [];
+    var data = [];
+
     for(var i = 0; i < allImages.length; i++){
-        var image = allImages[i];
-        var li = document.createElement("li");
-        li.textContent = image.description + " received " + image.clicks + " votes.";
-        ul.appendChild(li);
+        labels.push(allImages[i].description);
+        data.push(allImages[i].clicks);
     }
-    results.appendChild(ul);
+    createChart(context2d, labels, "Number of Votes", data);
+
+    var results = document.getElementById("results");
+    results.appendChild(canvas);
 }
 
-var ctx = document.getElementById('canvas').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: imageNames[2],
-        datasets: [{
-            label: 'Results',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: imageNames[2].totalClicks
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-showResults();
 createImages();
 render();
